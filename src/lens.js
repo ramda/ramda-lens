a1f99cd3baca206d32a3d22437784d2deddb991f
Brian Lonsdorf <brian@looprecur.com>
2016-01-17 14:44:33 -0800

    expose lens for extension

--- src/lens.js
+++ src/lens.js
@@ -1,6 +1,8 @@
 var set = require('../node_modules/ramda/src/set')
+var lens = require('../node_modules/ramda/src/lens')
 
 module.exports = {
+  lens: lens,
   set: set,
   view: require('./view'),
   over: require('./over')
