6e25ab5f5d0d14bb82517b1d5bd8215cacd018df
Scott Christopher <scott.christopher@finder.com>
2016-10-15 10:33:09 +1100

    Allow `set` to work with traversals

--- src/lens.js
+++ src/lens.js
@@ -1,10 +1,29 @@
-var set = require('ramda/src/set')
-var lens = require('ramda/src/lens')
+var ConstList = require('./internal/_const')([])
+var Identity  = require('./internal/_identity')
+var curry     = require('ramda/src/curry')
+var lens      = require('ramda/src/lens')
 
+var view = curry(function view(lens, x) {
+  return lens.call(ConstList, function(a) {
+    return ConstList([a])
+  })(x).value[0]
+})
+
+var over = curry(function over(lens, f, x) {
+  return lens.call(Identity, function(y) {
+    return Identity(f(y));
+  })(x).value;
+})
+
+var set = curry(function set(lens, v, x) {
+  return lens.call(Identity, function(_) {
+    return Identity(v);
+  })(x).value;
+})
 
 module.exports = {
   lens: lens,
   set: set,
-  view: require('./view'),
-  over: require('./over')
+  view: view,
+  over: over
 }
